8ff842892b1e831f0e1d5352859cb63326cba979
wwz09 <47264059+wwz09@users.noreply.github.com>
2023-12-15 10:43:50 +0800

    更新 URL.java

--- DIY/URL.java
+++ DIY/URL.java
@@ -1,9 +1,9 @@
 package com.github.tvbox.osc.bbox.constant;
 
 public class URL {
-    public static String DOMAIN_NAME_PROXY = "";
-    public static final String GITHUB_VERSION_PATH = "";
-    public static final String APK_PATH = "";
+    //public static String DOMAIN_NAME_PROXY = "";
+    //public static final String GITHUB_VERSION_PATH = "";
+    //public static final String APK_PATH = "";
     // public static final String DOMAIN_NAME_PROXY = "http://192.168.3.159:81";
     // public static final String GITHUB_VERSION_PATH = "/chfs/shared/Github_Project/box/version.json";
     // public static final String APK_PATH = "/chfs/shared/Github_Project/box/Release/app-release.apk";
