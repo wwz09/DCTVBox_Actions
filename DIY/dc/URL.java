daaa901ff4bf3e446b14415662030e78a957c433
wwz09 <47264059+wwz09@users.noreply.github.com>
2024-03-07 17:42:22 +0800

    main

--- DIY/dc/URL.java
+++ DIY/dc/URL.java
@@ -12,7 +12,7 @@ public class URL {
     // https://ghproxy.com/https://raw.githubusercontent.com//mlabalabala/TVResource/main/boxCfg/ori_source.json
     // https://raw.bunnylblbblbl.eu.org/https://raw.githubusercontent.com/mlabalabala/TVResource/main/boxCfg/ori_source.json
     // 默认线路
-    public static final String DEFAULT_API_URL = "https://agit.ai/wwz09/ubuntu/raw/branch/master/mr.json";
+    public static final String DEFAULT_API_URL = "https://agit.ai/wwz09/ubuntu/raw/branch/master/main.json";
     // public static final String DEFAULT_API_URL = "https://raw.githubusercontent.com/gaotianliuyun/gao/master/0821.json";
     // 默认仓库线路
     public static final String DEFAULT_STORE_API_URL = "https://agit.ai/wwz09/ubuntu/raw/branch/master/TXT/dc.json";
