914b17876a586ac6b631cc69f53474565ba093ee
wwz09 <47264059+wwz09@users.noreply.github.com>
2024-03-07 15:30:27 +0800

    c

--- DIY/dc/ApiDialog.java
+++ DIY/dc/ApiDialog.java
@@ -340,12 +340,12 @@ public class ApiDialog extends BaseDialog {
     }
 
     private void putDefaultApis(String url) {
-      //  URL.DOMAIN_NAME_PROXY = url;
+        URL.DOMAIN_NAME_PROXY = url;
         // 默认线路地址
         String defaultApiName = "默认线路";
-        String defaultApi = "https://agit.ai/wwz09/ubuntu/raw/branch/master/main.json";
+        String defaultApi = url + URL.DEFAULT_API_URL;
         // 默认仓库地址
-        String defaultStoreApi = "https://agit.ai/wwz09/ubuntu/raw/branch/master/dc.json";
+        String defaultStoreApi = url + URL.DEFAULT_STORE_API_URL;
 
         Map<String, String> defaultApiMap = Hawk.get(HawkConfig.API_MAP, new HashMap<>());
         defaultApiMap.put(defaultApiName, defaultApi);
